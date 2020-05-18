7d1c912f12653310411619d628cf5fcab6084421
palm.laz <hellazaro@gmail.com>
2020-05-18 10:03:49 +0100

    GhostCursor script
    added script for following cursor. but it seems to not be working properly as it was before

---
+++ g'Host/Assets/Scripts/GhostCursor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostCursor : MonoBehaviour
+{
+    [SerializeField] Rigidbody cursor;
+    [SerializeField] Rigidbody ghost;
+
+    Vector3 mousePos;
+    Vector3 worldPos;
+
+    [SerializeField] bool collision = false;
+    [SerializeField] float releaseTime = 0.2f;
+
+    void FixedUpdate()
+    {
+        MousePos();
+    }
+
+    void MousePos()
+    {
+        if (!collision)
+        {
+            mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.transform.position.z; //garantir que o mousePos.z = camPos.z para em baixo converter tudo em world coords. como e 2d, o z vai ser subtraido no ScreenToWorldPoint 
+
+            worldPos = Camera.main.ScreenToWorldPoint(mousePos); //converte a mousePos em coords do world
+            cursor.transform.position = worldPos;
+            ghost.transform.position = worldPos;
+        }
+    }
+
+    void OnCollisionEnter(Collision wall)
+    {
+        collision = true;        
+    }
+
+    private void OnCollisionExit(Collision wall)
+    {
+        collision = false;
+        StartCoroutine(ReturnToPos());
+    }
+
+    IEnumerator ReturnToPos()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        Vector3 direction = (cursor.transform.position - ghost.transform.position);
+        ghost.transform.position = direction.normalized * Time.deltaTime;
+    }
+}
